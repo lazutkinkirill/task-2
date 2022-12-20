4032ac87407c4c830388a915915c339bdd9b760b
lazutkinkirill <lazutkin_k@inbox.ru>
2022-12-20 18:26:39 +0300

    finish task 2

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+ 
+            if( a > b )
+            {
+                Console.WriteLine("число " + a + " max");
+                Console.WriteLine("число " + b + " min");
+
+            }
+            else if ( a < b )
+            {
+                Console.WriteLine("число " + b + " max");
+                Console.WriteLine("число " + a + " min");
+
+            }
